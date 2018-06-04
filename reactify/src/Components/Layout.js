bf57b0bd29a15bbcf3b27c9e8c169f706d2a27b1
shachar <shachar@neowize.com>
2018-06-04 18:05:12 +0300

    Added routing! moved the player from the layout into the playlist as well as the state and the different methods.

--- reactify/src/Components/Layout.js
+++ reactify/src/Components/Layout.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 import {Nav, Navbar, NavbarBrand, NavItem, NavLink} from "reactstrap";
-import Player from "./Player";
 import Playlists from "./Playlists";
 import Playlist from "./Playlist";
 import {Switch} from "react-router";
-import {Redirect, Route, withRouter} from "react-router-dom";
+import {Redirect, Route, Router} from "react-router-dom";
+import createHistory from 'history/createBrowserHistory';
+
+const history = createHistory();
 
 
 class Layout extends Component {
@@ -35,83 +37,37 @@ class Layout extends Component {
                     ]
                 }
             ],
-            songIndex: 0,
-            view: "Library"
         };
-        this.state.activePlaylist = this.state.playlists[0];
     }
 
-    nextSong = () => {
-        const {songIndex, activePlaylist} = this.state;
-        let nextIndex = songIndex < activePlaylist.songs.length - 1 ? songIndex + 1 : 0;
-        this.setState({
-            songIndex: nextIndex,
-        })
-    };
-
-    previousSong = () => {
-        const {songIndex, activePlaylist} = this.state;
-        let previousIndex = songIndex > 0 ? songIndex - 1 : activePlaylist.songs.length - 1;
-
-        this.setState({
-            songIndex: previousIndex,
-        })
-    };
-
-    chooseSong = (songIndex) => {
-        if (this.state.view === "Library") {
-            this.setState({
-                activePlaylist: this.state.playlists[0],
-            })
-        }
-        this.setState({
-            songIndex,
-        })
-    };
-
-    choosePlaylist = (playlistIndex) => {
-        this.setState({
-            activePlaylist: this.state.playlists[playlistIndex],
-            view: "Playlist",
-        }, () => {
-            this.chooseSong(0)
-        });
-    };
-
-    getView = () => {
-        switch (this.state.view) {
-            case "Playlists":
-                return <Playlists playlists={this.state.playlists} choosePlaylist={this.choosePlaylist}/>;
-            case "Playlist":
-                return <Playlist songs={this.state.activePlaylist.songs} chooseSong={this.chooseSong}
-                                 name={this.state.activePlaylist.name}/>;
-            default:
-                return <Playlist songs={this.state.playlists[0].songs} chooseSong={this.chooseSong}
-                                 name={this.state.playlists[0].name}/>;
-        }
-    };
-
-    selectView = (view) => {
-        this.setState({view});
-    };
-
     render() {
-        return <div>
+        return <Router history={history}>
+            <div>
             <Navbar color="light" light expand="md">
                 <NavbarBrand href="/">Reactify</NavbarBrand>
                 <Nav navbar>
                     <NavItem>
-                        <NavLink onClick={() => this.selectView("Library")}>Library</NavLink>
+                        <NavLink href="/">Library</NavLink>
                     </NavItem>
                     <NavItem>
-                        <NavLink onClick={() => this.selectView("Playlists")}>Playlists</NavLink>
+                        <NavLink href="/playlists">Playlists</NavLink>
                     </NavItem>
                 </Nav>
             </Navbar>
-            {this.getView()}
-            <Player currentSong={this.state.activePlaylist.songs[this.state.songIndex]} playNext={this.nextSong}
-                    playPrevious={this.previousSong}/>
-        </div>;
+            <Switch>
+                <Route path="/" exact
+                       render={() => (
+                           <Playlist songs={this.state.playlists[0].songs} name={this.state.playlists[0].name}/>
+                       )}/>
+                <Route path="/playlists" exact render={() => (<Playlists playlists={this.state.playlists}/>)}/>
+                <Route path="/playlists/:index" render={({match}) => (
+                    <Playlist songs={this.state.playlists[match.params.index].songs}
+                              name={this.state.playlists[match.params.index].name}/>
+                )}/>
+                <Redirect to="/"/>
+            </Switch>
+            </div>
+        </Router>;
     }
 }
 
--- reactify/src/Components/Playlist.js
+++ reactify/src/Components/Playlist.js
@@ -1,26 +1,58 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Song from "./Song";
 import PropTypes from "prop-types";
+import Player from "./Player";
 
 class Playlist extends Component {
-  render() {
-    const {name, songs, chooseSong} = this.props;
+    constructor(props) {
+        super(props);
+        this.state = {
+            songIndex: 0,
+        }
+    }
 
-    return <div>
-      <h2>{name}</h2>
-      {songs.map((song, index) => (
-        <div key={index} onClick={() => chooseSong(index)}>
-          <Song artist={song.artist} name={song.name}/>
-        </div>
-      ))}
-    </div>;
-  }
+    nextSong = () => {
+        const {songs} = this.props;
+        let nextIndex = this.state.songIndex < songs.length - 1 ? this.state.songIndex + 1 : 0;
+        this.setState({
+            songIndex: nextIndex,
+        })
+    };
+
+    previousSong = () => {
+        const {songs} = this.props;
+        let previousIndex = this.state.songIndex > 0 ? this.state.songIndex - 1 : songs.length - 1;
+
+        this.setState({
+            songIndex: previousIndex,
+        })
+    };
+
+    chooseSong = (songIndex) => {
+        this.setState({
+            songIndex,
+        })
+    };
+
+    render() {
+        const {name, songs} = this.props;
+
+        return <div>
+            <h2>{name}</h2>
+            {songs.map((song, index) => (
+                <div key={index} onClick={() => this.chooseSong(index)}>
+                    <Song artist={song.artist} name={song.name}/>
+                </div>
+            ))}
+            <Player currentSong={songs[this.state.songIndex]} playNext={this.nextSong}
+                    playPrevious={this.previousSong}/>
+        </div>;
+    }
 }
 
 Playlist.propTypes = {
-  name: PropTypes.string.isRequired,
-  songs: PropTypes.arrayOf(PropTypes.shape(Song.propTypes)),
-  chooseSong: PropTypes.func,
+    name: PropTypes.string.isRequired,
+    songs: PropTypes.arrayOf(PropTypes.shape(Song.propTypes)),
 };
 
 export default Playlist;
