2f56bb4c781fb25214835ed4728c0724e019218d
Alon Shenkler <alonshenk@gmail.com>
2018-06-04 13:43:00 +0300

    Player can play a song

--- reactify/src/Components/Library.js
+++ reactify/src/Components/Library.js
@@ -5,10 +5,11 @@ class Library extends Component {
   render() {
     return <Playlist songs={
       [
-        {artist: "The Rolling Stones", name: "Gimme Shelter"}, {
-        artist: "Red Hot Chili Peppers", name: "Californication"
-      }
-      ]}/>;
+        {artist: "Redbone", name: "Come and Get Your Love"},
+        {artist: "The Rolling Stones", name: "Gimme Shelter"},
+        {artist: "Red Hot Chili Peppers", name: "Californication"}
+      ]
+    }/>;
   }
 }
 
--- reactify/src/Components/Player.js
+++ reactify/src/Components/Player.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import comeAndGetYourLove from "../resources/ComeAndGetYourLove.mp3";
+
 class Player extends Component {
   render() {
-    return <div>
-
-    </div>;
+    return <audio controls>
+      <source src={comeAndGetYourLove} type="audio/ogg"/>
+      Your browser does not support the audio tag.
+    </audio>;
   }
 }
 
